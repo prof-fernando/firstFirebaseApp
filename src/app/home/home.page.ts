7b5875d13249c374b856165c65c26e14b4a8722c
Fernando luis <fernando.tetu@gmail.com>
2019-09-17 18:49:13 -0300

    add integração entre firebase e ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  constructor(private fb: AngularFireDatabase) {
+    fb.list('/alunos')
+      .valueChanges()
+      .subscribe(dados => {
+        console.log('atualizado ', dados);
+      });
+  }
 
-  constructor() {}
-
+  gravar() {
+    const dados = {
+      matricula: 123,
+      nome: 'Pedro',
+      sobrenome: 'Silva'
+    };
+    this.fb.list('/alunos').push(dados);
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  fireDefs: {
+    apiKey: 'AIzaSyA_6LRaY7171qgKvDWAGWtUEaZilPdkSb0',
+    authDomain: 'fbcad1234.firebaseapp.com',
+    databaseURL: 'https://fbcad1234.firebaseio.com',
+    projectId: 'fbcad1234',
+    storageBucket: 'fbcad1234.appspot.com',
+    messagingSenderId: '397496044204',
+    appId: '1:397496044204:web:af1e8c9e84f60fff256d09'
+  }
 };
 
 /*
